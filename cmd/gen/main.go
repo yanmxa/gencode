3ae6bb4bec14fae8ae39c18ee4c253493b1606f3
Meng Yan <myan@redhat.com>
2026-03-15 11:18:55 +0800

    chore: bump version to 1.9.7
    Signed-off-by: Meng Yan <myan@redhat.com>

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -23,7 +23,7 @@ import (
 	_ "github.com/yanmxa/gencode/internal/provider/alibaba"
 )
 
-var version = "1.9.6"
+var version = "1.9.7"
 
 // cliOpts holds all CLI flag values in one place.
 var cliOpts struct {
