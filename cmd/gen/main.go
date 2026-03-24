a2382574e33e2a08c4e54ce8b1c1212da0a827b8
Meng Yan <myan@redhat.com>
2026-03-24 09:39:37 +0800

    chore: bump version to 1.10.1
    Signed-off-by: Meng Yan <myan@redhat.com>

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -23,7 +23,7 @@ import (
 	_ "github.com/yanmxa/gencode/internal/provider/alibaba"
 )
 
-var version = "1.10.0"
+var version = "1.10.1"
 
 // cliOpts holds all CLI flag values in one place.
 var cliOpts struct {
