a7bcb5961dc8bea32d7dad10795bbaa811b6879a
Meng Yan <myan@redhat.com>
2026-04-22 14:01:26 +0800

    chore: bump version to 1.14.5
    Signed-off-by: Meng Yan <myan@redhat.com>

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -23,7 +23,7 @@ import (
 	_ "github.com/yanmxa/gencode/internal/llm/openai"
 )
 
-var version = "1.14.4"
+var version = "1.14.5"
 
 // cliOpts holds all CLI flag values in one place.
 var cliOpts struct {
